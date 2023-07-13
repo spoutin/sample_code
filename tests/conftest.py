42da3c73d567e5085ac6396170c8e9005d31daef
Achraf El Alaoui <achraf9431@gmail.com>
2023-07-13 01:50:16 +0100

    add tests for main file

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,9 @@ def mock_audit_mongo_client():
 
 @pytest.fixture(autouse=True)
 def mock_usage_mongo_run_agg_query():
-    mock_mongo_client = patch("sample_code.dao.audit.AuditDAO.run_aggregation_query")
-    yield mock_mongo_client.start()
-    mock_mongo_client.stop()
+    mock_run_query = patch("sample_code.dao.audit.AuditDAO.run_aggregation_query")
+    yield mock_run_query.start()
+    mock_run_query.stop()
 
 
 @pytest.fixture(autouse=True)
@@ -26,9 +26,9 @@ def mock_usage_mongo_client():
 
 @pytest.fixture(autouse=True)
 def mock_usage_mongo_run_query():
-    mock_mongo_client = patch("sample_code.dao.usage.UsageDAO.run_query")
-    yield mock_mongo_client.start()
-    mock_mongo_client.stop()
+    mock_run_query = patch("sample_code.dao.usage.UsageDAO.run_query")
+    yield mock_run_query.start()
+    mock_run_query.stop()
 
 
 @pytest.fixture(autouse=True)
@@ -36,3 +36,10 @@ def mock_report_mysql_client():
     mock_mysql_client = patch("sample_code.dao.reporting.create_engine")
     yield mock_mysql_client.start()
     mock_mysql_client.stop()
+
+
+@pytest.fixture()
+def mock_main_run_compare_on_node():
+    mock_compare_on_node = patch("sample_code.main.Main.run_compare_on_node")
+    yield mock_compare_on_node.start()
+    mock_compare_on_node.stop()
